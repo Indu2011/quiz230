8f5912ce0734d27521600b1de6f43ef3e4cec62d
Indu <indu.21bce9005@vitapstudent.ac.in>
2023-11-22 10:41:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "antd";
+import "./stylesheets/theme.css";
+import "./stylesheets/alignments.css";
+import "./stylesheets/textelements.css";
+import "./stylesheets/custom-components.css";
+import "./stylesheets/form-elements.css";
+import "./stylesheets/layout.css";
+import "./stylesheets/bg.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/common/Login";
+import Register from "./pages/common/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./pages/common/Home";
+import Exams from "./pages/admin/Exams";
+import AddEditExam from "./pages/admin/Exams/AddEditExam";
+import Loader from "./components/Loader";
+import { useSelector } from "react-redux";
+import WriteExam from "./pages/user/WriteExam";
+import UserReports from "./pages/user/UserReports";
+import AdminReports from "./pages/admin/AdminReports";
 
 function App() {
+  const { loading } = useSelector((state) => state.loader);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading && <Loader />}
+      <BrowserRouter>
+        <Routes>
+          {/* Common Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* User Routes */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/user/write-exam/:id"
+            element={
+              <ProtectedRoute>
+                <WriteExam />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/user/reports"
+            element={
+              <ProtectedRoute>
+                <UserReports />
+              </ProtectedRoute>
+            }
+          />
+          {/* Admin Routes */}
+          <Route
+            path="/admin/exams"
+            element={
+              <ProtectedRoute>
+                <Exams />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/exams/add"
+            element={
+              <ProtectedRoute>
+                <AddEditExam />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/admin/exams/edit/:id"
+            element={
+              <ProtectedRoute>
+                <AddEditExam />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/admin/reports"
+            element={
+              <ProtectedRoute>
+                <AdminReports />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/admin/Exams/AddEditQuestion.js
@@ -0,0 +1,119 @@
+import { Modal, Form, message } from 'antd'
+import React from 'react'
+import { useDispatch } from "react-redux";
+import { addQuestionToExam, editQuestionById } from "../../../apicalls/exams"
+import { HideLoading, ShowLoading } from "../../../redux/loaderSlice";
+
+function AddEditQuestion({
+    showAddEditQuestionModal,
+    setShowAddEditQuestionModal,
+    refreshData,
+    examId,
+    selectedQuestion,
+    setSelectedQuestion
+}) {
+
+    const dispatch = useDispatch();
+    const onFinish = async (values) => {
+        try {
+            dispatch(ShowLoading());
+            const requiredPayload = {
+                name: values.name,
+                correctOption: values.correctOption,
+                options: {
+                    A: values.A,
+                    B: values.B,
+                    C: values.C,
+                    D: values.D,
+                },
+                exam: examId,
+            };
+            let response
+            if (selectedQuestion) {
+                response = await editQuestionById({
+                    ...requiredPayload,
+                    questionId: selectedQuestion._id
+                })
+            }
+            else {
+                response = await addQuestionToExam(requiredPayload);
+            }
+            if (response.success) {
+                message.success(response.message);
+                refreshData();
+                setShowAddEditQuestionModal(false);
+            } else {
+                message.error(response.message);
+            }
+            setSelectedQuestion(null);
+            dispatch(HideLoading());
+        } catch (error) {
+            dispatch(HideLoading());
+            message.error(error.message);
+        }
+    }
+
+    return (
+        <Modal
+            title={selectedQuestion ? "Edit Question" : "Add Question"}
+            visible={showAddEditQuestionModal}
+            footer={false}
+            onCancel={() => {
+                setShowAddEditQuestionModal(false)
+                setSelectedQuestion(null)
+            }}
+        >
+            <Form onFinish={onFinish} layout="vertical" style={{backgroundColor:"#FDCEDF"}}
+                initialValues={{
+                    name: selectedQuestion?.name,
+                    A: selectedQuestion?.options?.A,
+                    B: selectedQuestion?.options?.B,
+                    C: selectedQuestion?.options?.C,
+                    D: selectedQuestion?.options?.D,
+                    correctOption: selectedQuestion?.correctOption,
+                }}
+            >
+                <Form.Item name="name" label="Question" >
+                    <input type="text" style={{ width: "89%" }} />
+                </Form.Item>
+                <Form.Item name="correctOption" label="Correct Option">
+                    <input type="text" style={{ width: "89%" }} />
+                </Form.Item>
+
+                <div className="flex gap-4">
+                    <Form.Item name="A" label="Option A">
+                        <input type="text" style={{ width: "90%" }} />
+                    </Form.Item>
+                    <Form.Item name="B" label="Option B">
+                        <input type="text" style={{ width: "90%" }} />
+                    </Form.Item>
+                </div>
+                <div className="flex gap-4">
+                    <Form.Item name="C" label="Option C">
+                        <input type="text" style={{ width: "90%" }} />
+                    </Form.Item>
+                    <Form.Item name="D" label="Option D">
+                        <input type="text" style={{ width: "90%" }} />
+                    </Form.Item>
+                </div>
+
+                <div className="flex justify-end mt-2 gap-4">
+                    <button
+                        className="primary-outlined-btn"
+                        type="button"
+                        onClick={() => setShowAddEditQuestionModal(false)}
+                    >
+                        Cancel
+                    </button>
+                    <button className="primary-contained-btn"
+                        onClick={() => setShowAddEditQuestionModal(true)}
+                    >
+                        Save
+                    </button>
+                </div>
+            </Form>
+        </Modal>
+    );
+}
+
+export default AddEditQuestion;
